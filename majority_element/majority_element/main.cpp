03edf2b3a48059faefb3c433d2ed735a5144306f
dxmvsh <siyezkan@strongte.am>
2023-02-23 20:23:51 +0600

    feat: add boyer-moore voting algorithm solution for majority element problem

--- majority_element/majority_element/main.cpp
+++ majority_element/majority_element/main.cpp
@@ -11,22 +11,37 @@
 #include "unordered_map"
 using namespace std;
 
+//int majorityElement(vector<int>& nums) {
+//    unordered_map<int, int> map;
+//    int element = 0;
+//    int maxOccurrences = -1;
+//    for(int i=0; i<nums.size(); i++) {
+//        map[nums[i]]++;
+//        if(map[nums[i]] > maxOccurrences) {
+//            element = nums[i];
+//            maxOccurrences = map[nums[i]];
+//        }
+//    }
+//    return element;
+//}
+
 int majorityElement(vector<int>& nums) {
-    unordered_map<int, int> map;
-    int element = 0;
-    int maxOccurrences = -1;
-    for(int i=0; i<nums.size(); i++) {
-        map[nums[i]]++;
-        if(map[nums[i]] > maxOccurrences) {
+    int count = 1, element = nums[0];
+    for(int i=1; i<nums.size(); i++) {
+        if(count == 0) {
             element = nums[i];
-            maxOccurrences = map[nums[i]];
+            count++;
+        } else if (element == nums[i]) {
+            count++;
+        } else {
+            count--;
         }
     }
     return element;
 }
 
 int main(int argc, const char * argv[]) {
-    vector<int> v {3,2,3};
+    vector<int> v {2,2,1,1,1,2,2};
     cout<<majorityElement(v)<<endl;
     return 0;
 }
