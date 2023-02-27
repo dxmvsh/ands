4bac71863e54ee79f37e93f16edad55dc67d12b9
Dimash <siyezkan@strongte.am>
2023-02-27 20:11:52 +0600

    feat: add better solution for valid anagram problem

--- valid_anagram/valid_anagram/main.cpp
+++ valid_anagram/valid_anagram/main.cpp
@@ -9,23 +9,38 @@
 #include "unordered_map"
 using namespace std;
 
+//bool isAnagram(string s, string t) {
+//    if(s.size() != t.size()) {
+//        return false;
+//    }
+//    unordered_map<char, int> charactersCount;
+//    for(int i=0; i<s.size(); i++) {
+//        if(!charactersCount[s[i]]) {
+//            charactersCount[s[i]] = 1;
+//        } else {
+//            charactersCount[s[i]] += 1;
+//        }
+//    }
+//    for(int i=0; i<t.size(); i++) {
+//        if(charactersCount[t[i]] <= 0) {
+//            return false;
+//        }
+//        charactersCount[t[i]]--;
+//    }
+//    return true;
+//}
 bool isAnagram(string s, string t) {
-    if(s.size() != t.size()) {
+    if(s.size() != t.size())
         return false;
+    int charCount[26]{0};
+    int n = (int) s.size();
+    for(int i=0; i<n; i++) {
+        charCount[s[i]-'a']++;
+        charCount[t[i]-'a']--;
     }
-    unordered_map<char, int> charactersCount;
-    for(int i=0; i<s.size(); i++) {
-        if(!charactersCount[s[i]]) {
-            charactersCount[s[i]] = 1;
-        } else {
-            charactersCount[s[i]] += 1;
-        }
-    }
-    for(int i=0; i<t.size(); i++) {
-        if(charactersCount[t[i]] <= 0) {
+    for(int i=0; i<26; i++) {
+        if(charCount[i])
             return false;
-        }
-        charactersCount[t[i]]--;
     }
     return true;
 }
