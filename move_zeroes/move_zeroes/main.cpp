314035bf34ae33db8466ecf02c1cac6295c3e381
dxmvsh <siyezkan@strongte.am>
2023-02-28 20:51:56 +0600

    feat: add better solution for move zeroes problem

--- move_zeroes/move_zeroes/main.cpp
+++ move_zeroes/move_zeroes/main.cpp
@@ -8,35 +8,48 @@
 #include <iostream>
 #include "vector"
 using namespace std;
+//void moveZeroes(vector<int>& nums) {
+//    int zeroesCount = 0;
+//    for(int i=0; i<nums.size(); i++) {
+//        if(nums[i])
+//            zeroesCount++;
+//    }
+//    int zeroIndex = -1;
+//    int nonZeroIndex = 0;
+//    int i = 0;
+//    while (i < nums.size() && zeroesCount != 0) {
+//        if(!nums[i]) {
+//            zeroIndex = i;
+//            nonZeroIndex = zeroIndex;
+//            while(nonZeroIndex < nums.size() && nums[nonZeroIndex] == 0)
+//                nonZeroIndex++;
+//            if(nonZeroIndex >= nums.size())
+//                return;
+//            if (nonZeroIndex != zeroIndex)
+//                swap(nums[zeroIndex], nums[nonZeroIndex]);
+//            zeroesCount--;
+//        }
+//        i++;
+//    }
+//    for(int a: nums)
+//        cout<<a<<" ";
+//    cout<<endl;
+//}
 void moveZeroes(vector<int>& nums) {
     int zeroesCount = 0;
     for(int i=0; i<nums.size(); i++) {
-        if(nums[i])
+        if(!nums[i])
             zeroesCount++;
-    }
-    int zeroIndex = -1;
-    int nonZeroIndex = 0;
-    int i = 0;
-    while (i < nums.size() && zeroesCount != 0) {
-        if(!nums[i]) {
-            zeroIndex = i;
-            nonZeroIndex = zeroIndex;
-            while(nonZeroIndex < nums.size() && nums[nonZeroIndex] == 0)
-                nonZeroIndex++;
-            if(nonZeroIndex >= nums.size())
-                return;
-            if (nonZeroIndex != zeroIndex)
-                swap(nums[zeroIndex], nums[nonZeroIndex]);
-            zeroesCount--;
-        }
-        i++;
+        else if (zeroesCount > 0)
+            swap(nums[i], nums[i - zeroesCount]);
     }
     for(int a: nums)
         cout<<a<<" ";
     cout<<endl;
 }
+
 int main(int argc, const char * argv[]) {
-    vector<int> v {1,0};
+    vector<int> v {0,1,0,2,12};
     moveZeroes(v);
     return 0;
 }
