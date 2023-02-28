219ccebf616a01988a0caa3459aa180570f4e6c6
dxmvsh <siyezkan@strongte.am>
2023-02-28 20:51:47 +0600

    feat: add solution for move zeroes problem

---
+++ move_zeroes/move_zeroes/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  move_zeroes
+//
+//  Created by Strong on 28.02.2023.
+//
+
+#include <iostream>
+#include "vector"
+using namespace std;
+void moveZeroes(vector<int>& nums) {
+    int zeroesCount = 0;
+    for(int i=0; i<nums.size(); i++) {
+        if(nums[i])
+            zeroesCount++;
+    }
+    int zeroIndex = -1;
+    int nonZeroIndex = 0;
+    int i = 0;
+    while (i < nums.size() && zeroesCount != 0) {
+        if(!nums[i]) {
+            zeroIndex = i;
+            nonZeroIndex = zeroIndex;
+            while(nonZeroIndex < nums.size() && nums[nonZeroIndex] == 0)
+                nonZeroIndex++;
+            if(nonZeroIndex >= nums.size())
+                return;
+            if (nonZeroIndex != zeroIndex)
+                swap(nums[zeroIndex], nums[nonZeroIndex]);
+            zeroesCount--;
+        }
+        i++;
+    }
+    for(int a: nums)
+        cout<<a<<" ";
+    cout<<endl;
+}
+int main(int argc, const char * argv[]) {
+    vector<int> v {1,0};
+    moveZeroes(v);
+    return 0;
+}
