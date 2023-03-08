05533ffdef87acce3db5f84c81f1e2bd6db44216
dxmvsh <siyezkan@strongte.am>
2023-03-08 18:26:06 +0600

    feat: add sort + 2 pointers solution for intersection of two arrays ii problem

--- intersection_of_two_arrays_ii/intersection_of_two_arrays_ii/main.cpp
+++ intersection_of_two_arrays_ii/intersection_of_two_arrays_ii/main.cpp
@@ -11,17 +11,19 @@
 using namespace std;
 
 vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-    if(nums2.size() < nums1.size())
-        intersect(nums2, nums1);
-    unordered_map<int, int> counter;
     vector<int> result;
-    for(int i=0; i<nums1.size(); i++) {
-        counter[nums1[i]]++;
-    }
-    for(int i=0; i<nums2.size(); i++) {
-        if(counter[nums2[i]]) {
-            counter[nums2[i]]--;
-            result.push_back(nums2[i]);
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end());
+    int i=0, j=0;
+    while (i<nums1.size() && j<nums2.size()) {
+        if(nums1[i] < nums2[j])
+            i++;
+        else if(nums1[i] > nums2[j])
+            j++;
+        else {
+            result.push_back(nums1[i]);
+            i++;
+            j++;
         }
     }
     return result;
