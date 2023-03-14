32c9f4a9d8ce5269f60764cfbb1221a13c80dae1
dxmvsh <siyezkan@strongte.am>
2023-03-14 21:50:41 +0600

    feat: add solution with hashing for group anagrams problem

--- group_anagrams/group_anagrams/main.cpp
+++ group_anagrams/group_anagrams/main.cpp
@@ -10,31 +10,26 @@
 #include "unordered_map"
 using namespace std;
 
-bool areAnagrams(string first, string second) {
-    if(second.size() != first.size())
-        return false;
-    sort(first.begin(), first.end());
-    sort(second.begin(), second.end());
-    return first == second;
-}
-
-bool hasGroupWithString(vector<vector<string>> v, string s) {
-    for(int i=0; i<v.size(); i++) {
-        if(areAnagrams(v[i][0], s))
-            return true;
+long long int hashString(string s) {
+    long long constant = 1e12;
+    vector<int> n = {2, 3, 5, 7, 11 ,13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 107};
+    long long int result = 1;
+    for(char c: s) {
+        int index = c - 'a';
+        result *= n[index];
+        result %= constant;
     }
-    return false;
+    return result;
 }
 
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
     vector<vector<string>> result;
     if(!strs.size())
         return result;
-    unordered_map<string, vector<string>> dictionary;
+    unordered_map<long long int, vector<string>> dictionary;
     for(int i=0; i<strs.size(); i++) {
         string s = strs[i];
-        sort(strs[i].begin(), strs[i].end());
-        dictionary[strs[i]].push_back(s);
+        dictionary[hashString(s)].push_back(s);
     }
     for(auto element: dictionary) {
         result.push_back(element.second);
@@ -43,7 +38,7 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
 }
 
 int main(int argc, const char * argv[]) {
-    vector<string> strs = { "eat","tea","tan","ate","nat","bat" };
+    vector<string> strs = { "ddddddddddg","dgggggggggg" };
     vector<vector<string>> groups = groupAnagrams(strs);
     for(vector<string> group: groups) {
         for(string element: group) {
