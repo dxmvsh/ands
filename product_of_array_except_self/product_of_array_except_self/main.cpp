2873cdbdaa830233118b3771703786c311e16ca3
Dimash <siyezkan@strongte.am>
2023-03-18 17:56:47 +0600

    feat: add better solution for product of array except self problem

--- product_of_array_except_self/product_of_array_except_self/main.cpp
+++ product_of_array_except_self/product_of_array_except_self/main.cpp
@@ -11,19 +11,16 @@ using namespace std;
 
 vector<int> productExceptSelf(vector<int>& nums) {
     int n = (int) nums.size();
-    vector<int> result(n,0), beforeProduct(n,0), afterProduct(n,0);
+    vector<int> result(n,0);
     int product = 1;
     for(int i=0; i<n; i++) {
-        beforeProduct[i] = product;
+        result[i] = product;
         product *= nums[i];
     }
     product = 1;
     for(int i=n-1; i>=0; i--) {
-        afterProduct[i] = product;
+        result[i] *= product;
         product *= nums[i];
-    }
-    for(int i=0; i<n; i++) {
-        result[i] = beforeProduct[i] * afterProduct[i];
     }
     return result;
 }
@@ -37,10 +34,3 @@ int main(int argc, const char * argv[]) {
     cout<<endl;
     return 0;
 }
-
-/*
-1   2   3  4 5
-1   1   2  6 24
-40  40  20 5 5
-
- */
